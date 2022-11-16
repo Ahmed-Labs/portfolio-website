94f372a30e7d1589270c7e7cc6e5f0d730d39642
Ahmed Mohamed <ahmedelhagjr@gmail.com>
2022-11-16 02:51:10 -0500

    add custom Lato font and custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        beige: {
+          500: "#F0E7DB",
+          400: "#F4EDE4",
+          300: "#F5F0E8"
+        },
+      },
+      fontFamily: {
+        Lato: ["Lato", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
