580499a5c6a29d8f8e5a9e14d4801eb85eb8c98e
Ahmed Mohamed <ahmedelhagjr@gmail.com>
2022-11-17 16:27:02 -0500

    created a projects component that maps and styles a list of projects

---
+++ components/projects.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+function Projects({ projects }) {
+  return (
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4">
+      {projects.map((project) => {
+        return (
+          <div className="flex flex-col items-cener">
+            <div className="h-[200px] w-full md:w-[250px] rounded-lg overflow-hidden">
+              <div className="h-full w-full relative bg-white">
+                <Image src={project.banner} layout="fill" objectFit="cover" />
+              </div>
+            </div>
+            <div className="p-2">
+              <Link
+                href={
+                  "/projects/" + project.name.replace(/\s+/g, "-").toLowerCase()
+                }
+              >
+                <p className="py-1 text-lg font-semibold">{project.name}</p>
+              </Link>
+              <p className="py-2">{project.description}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Projects;
