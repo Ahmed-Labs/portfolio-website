82b1a53ab1a15d43eacd0aab86d4e12f2f6b023e
Ahmed Mohamed <ahmedelhagjr@gmail.com>
2022-11-17 16:23:42 -0500

    create and complete next.js  404 page

---
+++ pages/404.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function FourOhFour() {
+  return (
+    <div className="w-full flex justify-center">
+      <div className="w-[600px] mx-5 flex flex-col items-center mt-10 md:my-20">
+        <div className="rounded-full align-middle w-[150px] h-[150px] border-4 border-beige-300 overflow-hidden mb-5">
+          <Image src="/unempfrog.avif" alt="avatar" width="200" height="200" />
+        </div>
+        <div className="text-center mx-3">
+          <p>
+            A human? What are you doing here? I believe you ended up in the
+            wrong place... I'm happy to have you here though! I don't get to see
+            any people most of the time... So, please feel free to stay or head
+            back if you must...
+          </p>
+        </div>
+        <Link href="/">
+          <div className="py-3 px-4 mt-5 bg-indigo-500 text-white rounded">
+            Head Back
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}
