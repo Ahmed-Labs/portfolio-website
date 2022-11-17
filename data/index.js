dafc321091104ea5cc89d75c489c0d32543ba391
Ahmed Mohamed <ahmedelhagjr@gmail.com>
2022-11-17 16:19:46 -0500

    Add data file with project info

---
+++ data/index.js
@@ -0,0 +1,87 @@
+export function getProjects() {
+  const projects = [
+    {
+      name: "Trackle",
+      banner: "/trackle1.png",
+      featured: true,
+      description:
+        "An all-in-one package tracking solution. Allows users to effortlessly track multiple shipments from different shipping carriers",
+      website: "https://trackle.vercel.app/dashboard",
+      stack: ["Next.js", "React.js", "Tailwind", "MongoDB", "RegEx"],
+      source: "https://github.com/Ahmed-Labs/trackle",
+      previews: [
+        "/trackleprev1.png",
+        "/trackleprev2.png",
+        "/trackleprev3.png",
+        "/trackleprev4.png",
+      ],
+    },
+    {
+      name: "Ethereum Multi-sender",
+      banner: "/eth.png",
+      featured: true,
+      description:
+        "This application allows users to send Ethereum to multiple addresses in a single transaction, saving time and substantially reducing transaction gas costs.",
+      website: "https://www.trackle.vercel.app/dashboard",
+      stack: ["Next", "React", "Tailwind", "MongoDB", "RegEx"],
+      source: "https://github.com/Ahmed-Labs/eth-multi-sender",
+      previews: [],
+    },
+    {
+      name: "StudyZone",
+      banner: "/study.webp",
+      featured: true,
+      description:
+        "An all-in-one package tracking solution. Allows users to effortlessly track multiple shipments from different shipping carriers",
+      website: "https://www.trackle.vercel.app/dashboard",
+      stack: ["Next", "React", "Tailwind", "MongoDB", "RegEx"],
+      source: "https://github.com/Ahmed-Labs/trackle",
+      previews: [],
+    },
+    {
+      name: "Route Mixer",
+      banner: "/gmap.jpg",
+      featured: true,
+      description:
+        "An all-in-one package tracking solution. Allows users to effortlessly track multiple shipments from different shipping carriers",
+      website: "https://www.trackle.vercel.app/dashboard",
+      stack: ["Next", "React", "Tailwind", "MongoDB", "RegEx"],
+      source: "https://github.com/Ahmed-Labs/trackle",
+      previews: [],
+    },
+    {
+      name: "Portfolio Site",
+      banner: "/gmap.jpg",
+      featured: false,
+      description:
+        "An all-in-one package tracking solution. Allows users to effortlessly track multiple shipments from different shipping carriers",
+      website: "https://www.trackle.vercel.app/dashboard",
+      stack: ["Next", "React", "Tailwind", "MongoDB", "RegEx"],
+      source: "https://github.com/Ahmed-Labs/trackle",
+      previews: [],
+    },
+    {
+      name: "Portfolio Site",
+      banner: "/gmap.jpg",
+      featured: false,
+      description:
+        "An all-in-one package tracking solution. Allows users to effortlessly track multiple shipments from different shipping carriers",
+      website: "https://www.trackle.vercel.app/dashboard",
+      stack: ["Next", "React", "Tailwind", "MongoDB", "RegEx"],
+      source: "https://github.com/Ahmed-Labs/trackle",
+      previews: [],
+    },
+    {
+      name: "Portfolio Site",
+      banner: "/gmap.jpg",
+      featured: false,
+      description:
+        "An all-in-one package tracking solution. Allows users to effortlessly track multiple shipments from different shipping carriers",
+      website: "https://www.trackle.vercel.app/dashboard",
+      stack: ["Next", "React", "Tailwind", "MongoDB", "RegEx"],
+      source: "https://github.com/Ahmed-Labs/trackle",
+      previews: [],
+    },
+  ];
+  return projects;
+}
