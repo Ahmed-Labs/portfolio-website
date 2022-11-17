bf34207277dfb6ef828fcb9b6529bf8d458abf7d
Ahmed Mohamed <ahmedelhagjr@gmail.com>
2022-11-17 16:33:56 -0500

    add components to home page, filter and map featured projects, add responsive styles

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,79 @@
 import Head from "next/head";
 import Image from "next/image";
-import { BsFillSunFill } from 'react-icons/bs';
+import Link from "next/link";
+import Technology from "../components/technology";
+import Projects from "../components/projects";
+import { getProjects } from "../data";
 
 export default function Home() {
+  const featuredProjects = getProjects().filter((project) => {
+    return project.featured;
+  });
   return (
-    <div className="font-Lato w-full h-full bg-beige-500">
-      <div className="sticky z-10 top-0 left-0 right-0 backdrop-blur bg-white/30 flex justify-center items-center">
-        <div className="flex items-center w-[700px] mx-2 py-1">
-          <div className="font-bold text-lg mx-4">Ahmed Mohamed</div>
-          <div className="ml-5">
-            <ul className="flex text-gray-600">
-              <li className="p-3 font-semibold">Works</li>
-              <li className="p-3 font-semibold">Source</li>
-            </ul>
+    <div className="mt-4 flex justify-center">
+      <div className="flex flex-col items-center max-w-[600px] mx-5 px-2">
+        {/* Header */}
+        
+        <div className="w-full flex items-center my-5">
+          <div>
+            <h1 className="text-2xl md:text-3xl font-semibold">Ahmed Mohamed</h1>
+            <h2 className="md:text-lg text-gray-500">
+              (Developer / Student / Tea enjoyer)
+            </h2>
           </div>
-          <div className="text-white bg-indigo-500 p-3 text-xl font-bold rounded ml-auto">
-            <BsFillSunFill />
+          <div className="ml-auto rounded-full align-middle w-[64px] h-[64px] md:w-[100px] md:h-[100px] border-4 border-beige-300 overflow-hidden">
+            <Image
+              src="/unempfrog.avif"
+              alt="avatar"
+              width="200"
+              height="200"
+            />
           </div>
         </div>
-      </div>
-      <div className="flex justify-center">
-        <div className="flex flex-col items-center relative max-w-[600px] mx-5 px-2">
-          {/* Header */}
-          <div className="w-full flex items-center my-5">
-            <div>
-              <h1 className="text-2xl font-bold">Ahmed Mohamed</h1>
-              <h2 className="text-lg font-semibold text-gray-500">
-                (Developer / Student / Tea enjoyer)
-              </h2>
-            </div>
-            <div className="ml-auto rounded-full align-middle w-[100px] h-[100px] border-4 border-white overflow-hidden">
-              <Image src="/avatar.jpg" alt="avatar" width="200" height="200" />
-            </div>
+        {/* Hero */}
+        <div className="relative overflow-hidden w-full my-3 px-5 py-5 rounded-lg bg-beige-300">
+          <div className="h-[200px] top-0 left-0 w-[12px] absolute bg-indigo-500"></div>
+          <p className="ml-2">
+            Hello, I'm a computer engineering student and aspiring software
+            developer based in Canada!
+          </p>
+        </div>
+        {/* <div>
+          <p className="w-full my-3 px-5 py-5 rounded bg-beige-300">
+            Hello, I'm a computer engineering student and aspiring software
+            developer based in Canada!
+          </p>
+        </div> */}
+        {/* Projects */}
+        <div className="w-full mt-5">
+          <h1 className="text-xl font-bold mb-5">Profile</h1>
+          <p className="w-full my-3 px-5 py-5 rounded bg-beige-300">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+            fugiat quaerat vero facere sapiente voluptatem, ullam explicabo
+            modi, accusantium, quo quis aperiam illum beatae voluptas laboriosam
+            porro cum reprehenderit esse. Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Magni fugiat quaerat vero facere
+            sapiente voluptatem, ullam explicabo modi, accusantium, quo quis
+            aperiam illum beatae voluptas laboriosam porro cum reprehenderit
+            esse.
+          </p>
+        </div>
+        {/* Projects */}
+        <div className="w-full mt-5">
+          <h1 className="text-xl font-bold mb-5">Works</h1>
+          <Projects projects={featuredProjects} />
+          <div className="flex justify-center">
+            <Link href="/projects">
+              <div className="my-5 py-3 px-4 bg-indigo-500 rounded text-white">
+                All Projects
+              </div>
+            </Link>
           </div>
-          {/* Hero */}
-          <div>
-            <p className="w-full my-3 px-5 py-3 rounded bg-beige-300">
-              Hello, I'm a computer engineering student and aspiring software
-              developer based in Canada!
-            </p>
-          </div>
-          {/* Technologies */}
-          <div className="w-full h-[1000px] my-5">
-            <h1 className="text-xl font-bold">Technologies</h1>
-          </div>
-          {/* Projects */}
-          <div>
-            <h1 className="pb-20">Projects</h1>
-          </div>
-          {/* Footer */}
-          <div>
-            <h1 className="pb-20">Footer</h1>
-          </div>
+        </div>
+        {/* Technologies */}
+        <div className="w-full mt-5">
+          <h1 className="text-xl font-bold mb-5">Technologies</h1>
+          <Technology />
         </div>
       </div>
     </div>
