c1be80413ffb2177235fab11229df6f7b990b676
Ahmed Mohamed <ahmedelhagjr@gmail.com>
2022-11-17 20:51:32 -0500

    add dark mode palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,16 @@ module.exports = {
         beige: {
           500: "#F0E7DB",
           400: "#F4EDE4",
-          300: "#F5F0E8"
+          300: "#F5F0E8",
+        },
+        dark: {
+          500: "#202023",
+          300: "#313134",
+          200: "525252",
         },
         indigo: {
-          500: "#6B9080"
-        }
+          500: "#6B9080",
+        },
       },
       fontFamily: {
         Lato: ["Lato", "sans-serif"],
